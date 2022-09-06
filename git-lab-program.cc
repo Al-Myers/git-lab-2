3bed7db4ab1d59fa8928fed87305be46c54f3716
Al-Myers <vm993117@ohio.edu>
2022-09-06 16:53:44 -0400

    git lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alfred Myers
+ *        Date: 09\06/2022
+ * Description: Simple program for Git lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
